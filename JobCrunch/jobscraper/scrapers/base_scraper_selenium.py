bed0d0c4214438d1a27821bc95953f7c31c6a03d
divij2510 <divij25mehta@gmail.com>
2025-04-28 03:05:07 +0530

    revised dockerfile for render and switched to use selenium driverless without GRID

--- JobCrunch/jobscraper/scrapers/base_scraper_selenium.py
+++ JobCrunch/jobscraper/scrapers/base_scraper_selenium.py
@@ -25,7 +25,6 @@ class BaseScraperSelenium(ABC):
         # Anti-detection configuration
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--window-size=1920,1080")
-        options.binary_location = os.getenv('CHROME_BIN')
         # Set user-agent through CDP
         # options.aadd_cdp_option("userAgent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36")
 
